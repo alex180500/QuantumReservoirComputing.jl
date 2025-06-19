03dab4e5ec599ec81833f50888639625b7dd4867
Alessandro Romancino <30732244+alex180500@users.noreply.github.com>
2025-06-19 14:42:26 +0200

    0.0.18: improved qrc_tools

--- src/qrc/qrc_tools.jl
+++ src/qrc/qrc_tools.jl
@@ -4,11 +4,13 @@ function qelm_compute_networks(
     nq::Integer=get_nsys(input_states[:, 1]);
     n_states::Integer=size(input_states, 2)
 ) where {T<:Number}
-    probs_t = Matrix{Float64}(undef, 2^nq, n_states)
+    evol_ψ = Λ * input_states
+    
+    probs_t = similar(input_states, Float64)
     mi_net_t = Matrix{Float64}(undef, binomial(nq, 2), n_states)
     entropy_t = Matrix{Float64}(undef, nq, n_states)
-    @threads for i in 1:n_states
-        ψ_t = Λ * view(input_states, :, i)
+    @inbounds @threads for i in 1:n_states
+        ψ_t = view(evol_ψ, :, i)
         get_probabilities!(view(probs_t, :, i), ψ_t)
         ρ = ψ_t * ψ_t'
         correlation_edgelist!(view(mi_net_t, :, i), ρ, mutual_info, nq)
@@ -19,14 +21,13 @@ end
 
 function qelm_compute(
     Λ::AbstractMatrix{T},
-    input_states::AbstractMatrix{T},
-    nq::Integer=get_nsys(input_states[:, 1]);
-    n_states::Integer=size(input_states, 2)
+    input_states::AbstractMatrix{T}
 ) where {T<:Number}
-    probs_t = Matrix{Float64}(undef, 2^nq, n_states)
-    @threads for i in 1:n_states
-        ψ_t = Λ * view(input_states, :, i)
-        get_probabilities!(view(probs_t, :, i), ψ_t)
+    evol_ψ = Λ * input_states
+    
+    probs_t = similar(input_states, Float64)
+    @inbounds for i in axes(evol_ψ, 2)
+        get_probabilities!(view(probs_t, :, i), view(evol_ψ, :, i))
     end
     return probs_t
 end
