1c81464ee7e005b214df4c961039c3c4ea40cbfd
Alessandro Romancino <30732244+alex180500@users.noreply.github.com>
2025-07-09 16:09:36 +0200

    feat: added `base` keyword argument for `mutual_info` and `vn_entropy`

--- src/quantum/correlations.jl
+++ src/quantum/correlations.jl
@@ -7,24 +7,25 @@ function concurrence(ρ::AbstractMatrix{T}) where {T<:Number}
 end
 
 # calculates the von Neumann entropy of a density matrix
-function vn_entropy(all_λ::AbstractVector{T}; tol::Real=eps(real(T))) where {T<:Real}
-    S = 0.0
+function vn_entropy(all_λ::AbstractVector{T}; base::Real=2, tol::Real=1e-15) where {T<:Real}
+    S = zero(T)
     @inbounds for λ in all_λ
         if λ > tol
-            S -= λ * log2(λ)
+            S -= λ * log(base, λ)
         end
     end
     return S
 end
 
-function vn_entropy(ρ::AbstractMatrix{T}; tol::Real=eps(real(T))) where {T<:Number}
+function vn_entropy(ρ::AbstractMatrix{T}; base::Real=2, tol::Real=1e-15) where {T<:Number}
     all_λ = eigvals(Hermitian(ρ))
-    return vn_entropy(all_λ; tol)
+    return vn_entropy(all_λ; base=base, tol=tol)
 end
 
 # calculates the mutual information of a bipartite density matrix
-function mutual_info(ρ::AbstractMatrix{T}; tol::Real=eps(real(T))) where {T<:Number}
+function mutual_info(ρ::AbstractMatrix{T}; base::Real=2, tol::Real=1e-15) where {T<:Number}
     ρ_A = eigvals_2(ptrace_2qubits(ρ, 1))
     ρ_B = eigvals_2(ptrace_2qubits(ρ, 2))
-    return vn_entropy(ρ_A; tol) + vn_entropy(ρ_B; tol) - vn_entropy(ρ; tol)
+    mi = vn_entropy(ρ_A; base, tol) + vn_entropy(ρ_B; base, tol) - vn_entropy(ρ; base, tol)
+    return ifelse(mi < tol, zero(real(T)), mi)
 end
